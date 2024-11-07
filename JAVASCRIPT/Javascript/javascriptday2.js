e2300b4aeba57df91c917c062a67cabfa80bd86e
saphaltaneupane <neupanesaphalta@gmail.com>
2024-11-07 16:57:40 +0545

    commit

--- JAVASCRIPT/Javascript/javascriptday2.js
+++ JAVASCRIPT/Javascript/javascriptday2.js
@@ -1,1 +1,4 @@
-document.getElementById('selector').innerHTML="hello world";
+document.getElementById('selector').innerHTML=" we are inside the external file";
+function clickme(){
+    document.getElementById('selector').innerHTML='Hello JavaScript';
+}
