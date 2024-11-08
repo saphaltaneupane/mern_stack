0ece2ab0040041406c70ccb33ec5be52835a6a1c
saphaltaneupane <neupanesaphalta@gmail.com>
2024-11-08 18:31:05 +0545

    okay

--- JAVASCRIPT/Javascript/javascriptday2.js
+++ JAVASCRIPT/Javascript/javascriptday2.js
@@ -1,4 +1,7 @@
 document.getElementById('selector').innerHTML=" we are inside the external file";
 function clickme(){
     document.getElementById('selector').innerHTML='Hello JavaScript';
+
 }
+ const collection= document.getElementsByClassName("example");
+ collection[0].innerHTML="hi";
